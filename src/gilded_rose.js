e3786c0cb6dc942be2cef22cb579d60f7fca15df
athisa-tw <athisa.laungvarunyoo@jetstar.com>
2022-06-09 11:05:11 +0700

    simplify Backstage passe case and other products case

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -29,15 +29,11 @@ class Shop {
             if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality < 50) {
                     this.items[i].quality = this.items[i].quality + 1;
-                    if (this.items[i].sellIn < 11) {
-                        if (this.items[i].quality < 50) {
-                            this.items[i].quality = this.items[i].quality + 1;
-                        }
+                    if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
+                        this.items[i].quality = this.items[i].quality + 1;
                     }
-                    if (this.items[i].sellIn < 6) {
-                        if (this.items[i].quality < 50) {
-                            this.items[i].quality = this.items[i].quality + 1;
-                        }
+                    if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
+                        this.items[i].quality = this.items[i].quality + 1;
                     }
                 }
                 this.items[i].sellIn = this.items[i].sellIn - 1;
@@ -46,14 +42,13 @@ class Shop {
                 }
                 continue;
             }
+
             if (this.items[i].quality > 0) {
                 this.items[i].quality = this.items[i].quality - 1;
             }
             this.items[i].sellIn = this.items[i].sellIn - 1;
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].quality > 0) {
-                    this.items[i].quality = this.items[i].quality - 1;
-                }
+            if (this.items[i].sellIn < 0 && this.items[i].quality > 0) {
+                this.items[i].quality = this.items[i].quality - 1;
             }
         }
 
